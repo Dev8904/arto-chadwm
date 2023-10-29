b4ead83094be4ed278848627c8848e93b4ad1d41
Dev8904 <jonathanmarshall.IT@pm.me>
2023-10-29 19:30:28 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#set -e
+
+##################################################################################################################
+#Author  : Jonathan Marshall
+#Github  : https://github.com/Dev8904
+###########################################################################################################################
+
+#Declaring our installed directory
+installed_dir=$(dirname "$(readlink -f "$(basename "$(pwd)")")")
+
+##################################################################################################################
+CNT="[\e[1;36mNOTE\e[0m]"
+COK="[\e[1;32mOK\e[0m]"
+CER="[\e[1;31mERROR\e[0m]"
+CAT="[\e[1;37mATTENTION\e[0m]"
+CWR="[\e[1;35mWARNING\e[0m]"
+CAC="[\e[1;33mACTION\e[0m]"
+INSTLOG="install.log"
+
+#Move function
+move_files() 
+{
+    local dest="$1"
+    shift  # Remove the destination from the arguments
+
+    # Remaining arguments are the source files/directories
+    for src in "$@"; do
+        # Determine whether to use sudo based on the destination path
+        if [[ "$dest" == "$HOME"* ]]; then
+            # Move the source to the destination
+            mv -v "$src" "$dest" >> "$INSTLOG" 2> /dev/null &
+        else
+            # Move the source to the destination using sudo
+            sudo mv -v "$src" "$dest" >> "$INSTLOG" 2> /dev/null &
+        fi
+    done
+}
+
+
+#Move configuration files
+dwm_config=(
+  "$installed_dir/root/usr/bin/"* "/usr/bin/"
+  "$installed_dir/root/usr/local/bin/"* "/usr/local/bin/"
+  "$installed_dir/root/usr/share/backgrounds/"* "/usr/share/backgrounds/"
+  "$installed_dir/root/usr/share/xsessions/"* "/usr/share/xsessions/"
+  "$installed_dir/root/etc/skel/.config/arto-chadwm/"* "$HOME/.config/"
+)
+
+echo "Moving files..."
+for FILES in "${dwm_config[@]}"; do
+    move_files "$FILES" 
+done
+sleep 5
+echo "Done. Installing..."
+
+cd $HOME/.config/arto-chadwm/chadwm/
+sudo make install
+
+echo "Dwm Install finished."
+
+
