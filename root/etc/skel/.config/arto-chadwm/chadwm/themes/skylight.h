685f21eb8afd8189384ad9680e308388be9c3917
Dev8904 <jonathanmarshall.IT@pm.me>
2023-10-31 12:36:31 +0100

    testing

--- root/etc/skel/.config/arto-chadwm/chadwm/themes/skylight.h
+++ root/etc/skel/.config/arto-chadwm/chadwm/themes/skylight.h
@@ -1,4 +1,4 @@
-static const char black[]       = "#212337";
+static const char black[]       = "#1e1f21";
 static const char white[]       = "#abafbf";
 static const char gray2[]       = "#6d8dad"; // unfocused window border
 static const char gray3[]       = "#545962";
@@ -10,4 +10,4 @@ static const char orange[]      = "#ff9668";
 static const char yellow[]      = "#ffd700";
 static const char pink[]        = "#da70d1";
 static const char pinky[]       = "#b5aaff";
-static const char col_borderbar[]  = "#99c1f1"; // inner border
+static const char col_borderbar[]  = "#212337"; // inner border
